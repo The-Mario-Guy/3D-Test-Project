2ba1e066d3ef3a980ab7da83d142f3694ce15dc3
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-09-05 12:54:47 -0700

    Update FPS.cs

--- 3D Test Project/Assets/Scenes/Roll A Ball Scenes/Scripts/FPS.cs
+++ 3D Test Project/Assets/Scenes/Roll A Ball Scenes/Scripts/FPS.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FPS : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 25;
     }
 
     // Update is called once per frame
